9341b3b27d98cb572281e4a198546660f417a679
jatin lachhwani <jatin.commit@gmail.com>
2023-06-23 16:44:28 +0530

    simple-struct example added

---
+++ serialize-deserialize-primitives/examples/1.simple-struct/simple-struct.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "./../../serialize-deserialize.h"
+
+typedef struct employee_t 
+{ 
+    int id;
+    char first_name[30];
+    char last_name[30];
+}employee_t;
+
+void serialize_employee_t(employee_t*obj,ser_buff_t*b){
+    // returning spl value for NULL
+    if(obj==NULL){
+        unsigned int sentinel = 0xFFFFFFFF;
+        serialize_data(b,(char*)&sentinel,sizeof(unsigned int));
+        return;
+    }
+    // if not null fill the buffer with struct data
+        serialize_data(b,(char*)&obj->id,sizeof(int));
+        serialize_data(b,(char*)obj->first_name,sizeof(char)*30);
+        serialize_data(b,(char*)obj->last_name,sizeof(char)*30);
+}
+
+employee_t* de_serialize_employee_t(ser_buff_t *b){
+    // check for null
+    unsigned int sentinel=0;
+    de_serialize_data((char*)&sentinel,b,sizeof(unsigned int));
+    if(sentinel==0xFFFFFFFF)
+        return NULL;
+        
+    // if not null rewind read point in buffer
+    serialize_buffer_skip(b,(int)sizeof(unsigned int)*-1);
+    // fill the struct from serialized data
+    employee_t * obj = calloc(1,sizeof(employee_t));
+    de_serialize_data((char*)&obj->id,b,sizeof(int));
+    de_serialize_data((char*)obj->first_name,b,30*sizeof(char));
+    de_serialize_data((char*)obj->last_name,b,30*sizeof(char));
+    return obj;
+}
+
+void display_employee_t(employee_t *emp){
+    printf("Employee ID: %d\n", emp->id);
+    printf("First Name: %s\n", emp->first_name);
+    printf("Last Name: %s\n", emp->last_name);
+}
+
+int main(){
+    employee_t *emp1=calloc(1,sizeof(employee_t));
+    emp1->id=12;
+    strcpy(emp1->first_name,"Leonardo");
+    strcpy(emp1->last_name,"Dicaprio");
+
+    ser_buff_t *buff;
+    init_serialized_buffer(&buff);
+
+    serialize_employee_t(emp1,buff);
+    reset_serialize_buffer(buff);
+    employee_t *emp2 = de_serialize_employee_t(buff);
+
+    display_employee_t(emp1);
+    display_employee_t(emp2);
+
+    free(emp1);
+    free(emp2);
+
+    return 0;
+}
---
+++ serialize-deserialize-primitives/serialize-deserialize.c
@@ -0,0 +1,54 @@
+#include "serialize-deserialize.h"
+#include<stdio.h>
+#include<memory.h>
+#include<stdlib.h>
+#include<assert.h>
+
+void init_serialized_buffer(ser_buff_t **s_buff){
+    *s_buff = (ser_buff_t *)calloc(1,sizeof(ser_buff_t));
+    (*s_buff)->b =calloc(1,SERIALIZE_BUFFER_DEFAULT_SIZE);
+    (*s_buff)->size = SERIALIZE_BUFFER_DEFAULT_SIZE;
+    (*s_buff)->next= 0;
+}
+void init_serialized_buffer_of_defined_size(ser_buff_t **b, int size){
+    (*b) = (ser_buff_t *)calloc(1, sizeof(ser_buff_t));
+    (*b)->b = calloc(1, size);
+    (*b)->size = size;
+    (*b)->next = 0;
+}
+
+void serialize_data(ser_buff_t *b, char *data,int size){
+    int available_mem = b->size-b->next;
+    char isResize=0;
+
+    while(size > available_mem){
+        b->size  = b->size*2;
+        available_mem = b->size-b->next;
+        isResize=1;
+    }
+
+    if(isResize){
+        b->b = realloc(b->b,b->size);
+    }
+    memcpy(b->b+b->next,data,size);
+    b->next+=size;
+}
+
+void de_serialize_data(char *dest, ser_buff_t *b,int size){
+    memcpy(dest,b->b+b->next,size);
+    b->next+=size;
+}
+
+void serialize_buffer_skip(ser_buff_t*b,int skip_size){
+    if(b->next + skip_size >=0 && b->next+skip_size < b->size){
+        b->next+=skip_size;
+    }
+}
+
+void reset_serialize_buffer(ser_buff_t*b){
+    b->next=0;
+}
+
+int get_serialize_buffer_length(ser_buff_t *b){
+	return b->size;
+}
